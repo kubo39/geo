e58ac4c0522bcfcd6fcb101135a890cadf0aa9b0
Hiroki Noda <kubo39@gmail.com>
2021-12-20 05:34:35 +0900

    Implements intersects for Point, incomplete

--- source/geo/algorithm/intersects.d
+++ source/geo/algorithm/intersects.d
@@ -15,3 +15,21 @@ unittest
     assert(intersects(coord1, coord1));
     assert(!intersects(coord1, coord2));
 }
+
+bool intersects(T, U)(T lhs, U rhs)
+    if (isGeometry!T && is(U UU : Coordinate!real))
+{
+    static if (is(T TT : Point!real))
+    {
+        return lhs.coord.intersects(rhs);
+    }
+    else static assert(false, "Not implemented yet.");
+}
+
+unittest
+{
+    auto point = Point!float(1.0, 2.0);
+    auto coord = Coordinate!float(1.0, 2.0);
+    assert(intersects(point, coord));
+    assert(!intersects(point, -coord));
+}
