3917603f6c17360019f34d69d68203fd4bf081b3
Hiroki Noda <kubo39@gmail.com>
2021-12-26 11:35:31 +0900

    Fix: typo

--- source/geo/algorithm/kernels.d
+++ source/geo/algorithm/kernels.d
@@ -1,6 +1,7 @@
 module geo.algorithm.kernels;
 
 import std.traits : isNumeric;
+import geo.types;
 
 
 enum Orientation
@@ -16,7 +17,7 @@ enum Orientation
 Orientation orient2d(T)(Coordinate!T p, Coordinate!T q, Coordinate!T r)
     if (isNumeric!T)
 {
-    const res = (q.x - p.x) * (r.y - q.y) - (q.y - p,y) * (r.x - q.x);
+    const res = (q.x - p.x) * (r.y - q.y) - (q.y - p.y) * (r.x - q.x);
     if (res > cast(T) 0)
         return Orientation.CounterClockwise;
     else if (res < cast(T) 0)
